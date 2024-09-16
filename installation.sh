d1cefb92592ac4d2ea4893bef12919c422f8570c
kanth-sri <srikanthadepu8008@gmail.com>
2024-09-16 12:37:13 +0530

    commiting installation file

--- installation.sh
+++ installation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-echo "User id is :$USER_ID"
+
+if( $USER_ID -ne 0 )
+then 
+    echo "Please execute this script with root priviliges..."
+    exit 1
+fi
+
+dnf list installed git
