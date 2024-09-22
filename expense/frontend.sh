6473832b656379b91ed3eea7250a7ec5f4b7ae59
kanth-sri <srikanthadepu8008@gmail.com>
2024-09-22 15:12:16 +0530

    configuring frontend

---
+++ expense/frontend.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOG_FOLDER=/var/log/expense
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USER_ID -ne 0 ]
+    then
+        echo -e " $R Please execute this script with root priviliges... $N" 
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is....$G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is....$R FAILED $N" | tee -a $LOG_FILE
+    fi
+}
+CHECK_ROOT
+mkdir -p $LOG_FOLDER
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Unzip frontend code"
+
+#cp /etc/nginx/default.d/expense.conf
